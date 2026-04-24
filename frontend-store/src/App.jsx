8680e604350ad6a8be2f17d3548a45442b3e84a4
okureanthonytonny-commit <okureanthonytonny@gmail.com>
2026-04-24 20:35:14 +0300

    feat(App): - Added imports for Home, Shop, ProductDetail, Cart, Orders, OrderDetail  - Removed wrapper repetition of UserProvider and CartProvider  - Removed placeholder functions for the pages to connect pages to app

--- frontend-store/src/App.jsx
+++ frontend-store/src/App.jsx
@@ -1,35 +1,27 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavigationBar from './components/NavigationBar';
-import { UserProvider } from './context/UserContext';
-import { CartProvider } from './context/CartContext';
-
-// Temporary placeholder pages (we will replace these later)
-const Home = () => <h2 className='text-center mt-1'>Home page</h2>;
-const Shop = () => <h2 className='text-center mt-1'>Shop page</h2>;
-const ProductDetail = () => <h2 className='text-center mt-1'>Product Detail page</h2>;
-const Cart = () => <h2 className='text-center mt-1'>Cart page</h2>;
-const Orders = () => <h2 className='text-center mt-1'>Orders page</h2>;
-const OrderDetail = () => <h2 className='text-center mt-1'>Order Detail page</h2>;
+import Home from './pages/Home';
+import Shop from './pages/Shop';
+import ProductDetail from './pages/ProductDetail';
+import Cart from './pages/Cart';
+import Orders from './pages/Orders';
+import OrderDetail from './pages/OrderDetail';
 
 function App() {
   return (
-    <UserProvider>
-      <CartProvider>
-        <Router>
-          <NavigationBar />
-          <main className="container mt-4">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/shop" element={<Shop />} />
-              <Route path="/product/:id" element={<ProductDetail />} />
-              <Route path="/cart" element={<Cart />} />
-              <Route path="/orders" element={<Orders />} />
-              <Route path="/orders/:id" element={<OrderDetail />} />
-            </Routes>
-          </main>
-        </Router>
-      </CartProvider>
-    </UserProvider>
+    <Router>
+      <NavigationBar />
+      <main className="container mt-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/product/:id" element={<ProductDetail />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/orders/:id" element={<OrderDetail />} />
+        </Routes>
+      </main>
+    </Router>
   );
 }
 
